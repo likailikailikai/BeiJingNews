0b9a7ef4cf928f069a9d422a8c6582a8560cbe31
likailikailikai <1360869846@qq.com>
2017-02-07 13:17:43 +0800

    顶部新闻ViewPager数据的绑定

--- app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
@@ -1,9 +1,15 @@
 package com.atguigu.beijingnews.detailpager;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.atguigu.beijingnews.R;
 import com.atguigu.beijingnews.adapter.TabDetailPagerAdapter;
@@ -11,6 +17,8 @@ import com.atguigu.beijingnews.base.MenuDetailBasePager;
 import com.atguigu.beijingnews.bean.NewsCenterBean;
 import com.atguigu.beijingnews.bean.TabDetailPagerBean;
 import com.atguigu.beijingnews.utils.Constants;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.Gson;
 
 import org.xutils.common.Callback;
@@ -32,6 +40,11 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     @InjectView(R.id.listview)
     ListView listview;
+
+    ViewPager viewpager;
+    TextView tvTitle;
+    LinearLayout llGroupPoint;
+
     private String url;
     private TabDetailPagerAdapter adapter;
     /**
@@ -39,6 +52,10 @@ public class TabDetailPager extends MenuDetailBasePager {
      */
     private List<TabDetailPagerBean.DataEntity.NewsEntity> news;
 
+    /**
+     * 顶部轮播图的数据
+     */
+    private List<TabDetailPagerBean.DataEntity.TopnewsEntity> topnews;
 
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
@@ -51,6 +68,13 @@ public class TabDetailPager extends MenuDetailBasePager {
         //图组详情页面的视图
         View view = View.inflate(mContext, R.layout.tab_detail_pager, null);
         ButterKnife.inject(this, view);
+        View headerView = View.inflate(mContext, R.layout.header_view, null);
+        viewpager = (ViewPager) headerView.findViewById(R.id.viewpager);
+        tvTitle = (TextView) headerView.findViewById(R.id.tv_title);
+        llGroupPoint = (LinearLayout) headerView.findViewById(R.id.ll_group_point);
+
+        listview.addHeaderView(headerView);
+
         return view;
     }
 
@@ -96,6 +120,72 @@ public class TabDetailPager extends MenuDetailBasePager {
         //设置适配器
         adapter = new TabDetailPagerAdapter(mContext, news);
         listview.setAdapter(adapter);
+
+        //设置顶部新闻（轮播图）
+
+        //设置Viewpager的适配器
+        topnews = pagerBean.getData().getTopnews();
+        viewpager.setAdapter(new MyPagerAdapter());
+        //监听ViewPager页面的变化
+        viewpager.addOnPageChangeListener(new MyOnPageChangeListener());
+        tvTitle.setText(topnews.get(0).getTitle());
+
+
+    }
+
+    class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            tvTitle.setText(topnews.get(position).getTitle());
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+
+    class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return topnews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(mContext);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            //设置默认的和联网请求
+            //加载图片
+            Glide.with(mContext).load(Constants.BASE_URL + topnews.get(position).getTopimage())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    //设置默认图片
+                    .placeholder(R.drawable.news_pic_default)
+                    //请求图片失败
+                    .error(R.drawable.news_pic_default)
+                    .into(imageView);
+            //添加到ViewPager和返回
+            container.addView(imageView);
+
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
 
     }
 }
