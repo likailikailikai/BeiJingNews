1ed40c3db9f80e37d8356aad82e932867aa8eabb
likailikailikai <1360869846@qq.com>
2017-02-10 20:14:50 +0800

    极光推送集成到项目中

--- app/src/main/java/com/atguigu/beijingnews/MyApplication.java
+++ app/src/main/java/com/atguigu/beijingnews/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
 
 /**
  * Created by Baby on 2017/2/6.
@@ -15,5 +17,8 @@ public class MyApplication extends Application {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
