34417fc53fafb92cc0da40bc49bed13df1c4faca
情v枫 <1360869846@qq.com>
2017-02-13 23:34:52 +0800

    屏幕适配-尺寸(dimension)适配

--- ScreenAdapter_dimension/src/main/java/com/example/screenadapter_dimension/MainActivity.java
+++ ScreenAdapter_dimension/src/main/java/com/example/screenadapter_dimension/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.screenadapter_dimension;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        float density  = getResources().getDisplayMetrics().density;
+        Log.e("TAG","density=="+density);
+        Toast.makeText(this, "density=="+density, Toast.LENGTH_SHORT).show();
     }
 }
